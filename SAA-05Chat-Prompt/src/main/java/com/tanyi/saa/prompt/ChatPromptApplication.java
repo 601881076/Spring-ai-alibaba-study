e40184e8088925a9e58551460c280669ea382294
tanyi <601881076@qq.com>
2026-09-20 23:57:17 +0800

    feat: chatClient

---
+++ SAA-05Chat-Prompt/src/main/java/com/tanyi/saa/prompt/ChatPromptApplication.java
@@ -0,0 +1,17 @@
+package com.tanyi.saa.prompt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description 启动类
+ * Author
+ * Date 2026/9/20
+ * Version 1.0
+ **/
+@SpringBootApplication
+public class ChatPromptApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatPromptApplication.class, args);
+    }
+}
---
+++ SAA-05Chat-Prompt/src/main/java/com/tanyi/saa/prompt/controller/PromptController.java
@@ -0,0 +1,68 @@
+package com.tanyi.saa.prompt.controller;
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@Slf4j
+public class PromptController {
+    @Resource(name = "deepseek")
+    private ChatModel deepseekChatModel;
+    @Resource(name = "qwen")
+    private ChatModel qwenChatModel;
+
+    @Resource(name = "deepseekChatClient")
+    private ChatClient deepseekChatClient;
+    @Resource(name = "qwenChatClient")
+    private ChatClient qwenChatClient;
+
+    // http://localhost:8081/prompt/chat?question=火锅介绍下
+    @GetMapping("/prompt/chat")
+    public Flux<String> chat(@RequestParam(name = "question", defaultValue = "你是谁") String question) {
+        return deepseekChatClient.prompt()
+                .system("你是一个法律助手，只回答法律问题，其它问题回复，我只能回答法律相关问题，其它无可奉告")
+                .user(question)
+                .stream().content();
+
+    }
+
+    // http://localhost:8081/prompt/chat2?question=葫芦娃
+    @GetMapping("/prompt/chat2")
+    public Flux<ChatResponse> chat2(@RequestParam(name = "question", defaultValue = "你是谁") String question) {
+        // 系统消息
+        SystemMessage systemMessage = new SystemMessage("你是一个讲故事的助手，每个故事控制在300字以内");
+
+        // 用户消息
+        UserMessage userMessage = new UserMessage(question);
+
+        // 构建提示词
+        Prompt prompt = new Prompt(systemMessage, userMessage);
+
+        return deepseekChatModel.stream(prompt);
+    }
+
+    // http://localhost:8081/prompt/chat3?question=葫芦娃
+    @GetMapping("/prompt/chat3")
+    public Flux<String> chat3(@RequestParam(name = "question", defaultValue = "你是谁") String question) {
+        // 系统消息
+        SystemMessage systemMessage = new SystemMessage("你是一个讲故事的助手，每个故事控制在300字以内");
+        // 用户消息
+        UserMessage userMessage = new UserMessage(question);
+        // 构建提示词
+        Prompt prompt = new Prompt(systemMessage, userMessage);
+
+        // 构建响应
+        return deepseekChatModel.stream(prompt).mapNotNull(response -> response.getResults().getFirst().getOutput().getText());
+    }
+
+}
